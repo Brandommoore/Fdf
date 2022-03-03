864c19ef76881a8350956f096e1e09c339f8fc15
Brandom Moore <brandommoore@gmail.com>
2022-03-03 21:39:48 +0100

    another bressman change

--- utils/print_in_window/bresen_alg.c
+++ utils/print_in_window/bresen_alg.c
@@ -6,7 +6,7 @@
 /*   By: mcordoba <mcordoba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:07:46 by mcordoba          #+#    #+#             */
-/*   Updated: 2022/03/03 21:36:44 by mcordoba         ###   ########.fr       */
+/*   Updated: 2022/03/03 21:39:25 by mcordoba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,11 @@ void bresen_alg(float x, float y, float x1, float y1, t_data *fdf)
 	float	step_y;
 	int		max;
 
-	step_x = x1 - x;
-	step_y = y1 - y;
+	//step_x = x1 - x;
+	//step_y = y1 - y;
 
-	max = fmax(fmod((float)step_x), fmod((float)step_x));
-	step_x /= max;
-	step_x /= max;
+	//step_x /= max;
+	//step_x /= max;
 
 	(void)fdf;
 }
